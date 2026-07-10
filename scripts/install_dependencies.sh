1e7de8fe7568da53871e343e68883ee20cbed513
sagar10x <saisagar9305@gmail.com>
2026-07-10 19:37:31 +0530

    Update dependency installation script
    Replaced yum with dnf for Python 3 installation and removed unnecessary cd command.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-cd /home/ec2-user/aws-native-cicd-project
-
-sudo yum update -y
-
-sudo yum install python3 -y
-
+sudo dnf install -y python3
 python3 -m pip install --upgrade pip
-
 pip3 install -r requirements.txt
+
+sudo chown -R ec2-user:ec2-user /home/ec2-user/aws-native-cicd-project
+chmod -R 755 /home/ec2-user/aws-native-cicd-project
